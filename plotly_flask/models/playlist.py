55522d7cb2f300278a8ab18301210c25e75c50a2
Mihaela Aleksandrova <74715450+mihaelaaleks@users.noreply.github.com>
2024-01-06 18:32:36 +0100

    added playlist model and object

---
+++ plotly_flask/models/playlist.py
@@ -0,0 +1,8 @@
+class Playlist:
+    def __init__(self, name, playlist_id, image):
+        self.name = name
+        self.id = playlist_id
+        self.image = image
+        
+        
+    
---
+++ plotly_flask/models/playlist_logic.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pathlib import Path
+from plotly_flask.models.playlist import Playlist
+
+file_dir_path = Path(__file__).parent.parent.parent / Path('data')
+
+playlist_file_name = Path('my_cleaned_playlist.pkl')
+
+playlist_path = file_dir_path / playlist_file_name
+
+def df_to_playlist_obj():
+    playlist_df = pd.read_pickle(playlist_path)
+    playlist_obj_list = []
+    for index, row in playlist_df.iterrows():
+        playlist_name = row['name']
+        playlist_id = row['id']
+        playlist_image = row['images']
+        playlist = Playlist(name=playlist_name, playlist_id=playlist_id, image = playlist_image)
+        playlist_obj_list.append(playlist)
+    return playlist_obj_list

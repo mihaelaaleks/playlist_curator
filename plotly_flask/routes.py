b4429a7244b4f53d2c33e0b16f9cc4f79fd8ed67
Mihaela Aleksandrova <74715450+mihaelaaleks@users.noreply.github.com>
2024-04-02 20:47:37 +0200

    added dashboard route, templates and template components

--- plotly_flask/routes.py
+++ plotly_flask/routes.py
@@ -1,4 +1,5 @@
 """Routes for parent Flask app."""
+
 from flask import render_template, flash, redirect, url_for, request
 from flask import current_app as app
 
@@ -8,44 +9,67 @@ from plotly_flask.models import curator_logic
 from .forms import CuratorForm
 
 
-@app.route('/')
+@app.route("/")
 def home():
     """Landing page."""
     playlist_list = playlist_logic.df_to_playlist_obj()
     return render_template(
-        'index.html',
-        title='Plotly Dash Flask Tutorial',
-        description='Embed Plotly Dash into your Flask applications.',
-        template='home-template',
-        playlists = playlist_list,
-        body="This is a homepage served with Flask."
+        "index.html",
+        title="Plotly Dash Flask Tutorial",
+        description="Embed Plotly Dash into your Flask applications.",
+        template="index",
+        playlists=playlist_list,
+        body="This homepage is served with Flask.",
     )
 
 
-@app.route('/curator', methods=['GET', 'POST'])
+@app.route("/dashboard", methods=["GET", "POST"])
+def show_playlist_dashboard():
+    """Playlist statistics page."""
+    playlist_list = playlist_logic.df_to_playlist_obj()
+    return render_template(
+        "dashboard.html", playlists=playlist_list, template="info-template"
+    )
+
+
+@app.route("/dashboard/<playlist_id>", methods=["GET", "POST"])
+def show_playlist(playlist_id):
+    tracklist = playlist_logic.get_tracklist(playlist_id=playlist_id)
+    playlist_list = playlist_logic.df_to_playlist_obj()
+    avg_popularity = playlist_logic.get_avg_popularity(tracklist)
+    return render_template(
+        "dashboard.html",
+        tracklist=tracklist,
+        playlist_id = playlist_id,
+        playlists=playlist_list,
+        avg = avg_popularity,
+    )
+
+
+@app.route("/curator", methods=["GET", "POST"])
 def curator():
-    """Playlist curator page.""" 
+    """Playlist curator page."""
     playlist_list = playlist_logic.df_to_playlist_obj()
     form = CuratorForm(request.form)
     track_recommendations = []
     if request.method == "POST":
-        
+
         spotify = curator_logic.create_spotify()
-        track_recommendations = curator_logic.get_cleaned_recommendations(limit = 50,
-                                               spotify= spotify,
-                                               t_acousticness= form.acousticness.data,
-                                               t_danceability=form.danceability.data,
-                                               t_energy=form.energy.data,
-                                               t_instrumentalness=form.instrumentalness.data)
-        return render_template('recommended_panel.html',
-                               track_recommendations = track_recommendations)
+        track_recommendations = curator_logic.get_cleaned_recommendations(
+            limit=50,
+            spotify=spotify,
+            t_acousticness=form.acousticness.data,
+            t_danceability=form.danceability.data,
+            t_energy=form.energy.data,
+            t_instrumentalness=form.instrumentalness.data,
+        )
+        return render_template(
+            "recommended_panel.html", track_recommendations=track_recommendations
+        )
     return render_template(
-        'curator.html',
-        title='Plotly Dash Flask Tutorial',
-        description='Embed Plotly Dash into your Flask applications.',
-        playlists = playlist_list,
-        track_recommendations = track_recommendations,
-        form = form,
-        template='curator-template',
-        body="This is a homepage served with Flask."
+        "curator.html",
+        playlists=playlist_list,
+        track_recommendations=track_recommendations,
+        form=form,
+        template="curator-template",
     )

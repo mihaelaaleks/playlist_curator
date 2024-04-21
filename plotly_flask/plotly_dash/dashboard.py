ef5a65897ec1bb7df36beb060f362016b4445576
Mihaela Aleksandrova <74715450+mihaelaaleks@users.noreply.github.com>
2024-04-21 18:15:13 +0200

    Feature/gui readibility improvements (#30)
    * changed theme and added dashboard constants
    
    * removed additional scroll on dash embedding iframe
    
    * added styling elements for curator and dashboard containers
    
    * added sass changes
    
    * update footer name and link
    
    * restructured html for curator and dashboard

--- plotly_flask/plotly_dash/dashboard.py
+++ plotly_flask/plotly_dash/dashboard.py
@@ -2,7 +2,8 @@ from dash import Dash, html, dcc, Input, Output
 from plotly_flask.models import playlist_logic
 import plotly.express as px
 
-PLAYLIST_ID = "7doP9xG2n6DXnflGTmURsC"
+PLOTLY_THEME = "plotly_dark"
+CHART_TITLE = "Artist Counts"
 
 
 def create_dashboard(server):
@@ -14,16 +15,10 @@ def create_dashboard(server):
         ],
     )
 
-    # load dataframe
-    # df = playlist_logic.get_tracklist_w_labels(PLAYLIST_ID)
-    # artist_counts = df['artist_name'].value_counts()
-    # fig = px.pie(names=artist_counts.index, values=artist_counts.values, title='Artist Counts')
-    # fig.update_traces(textposition='inside', textinfo='percent+label', hoverinfo='label+percent')
-    # create dash layout
     dash_app.layout = html.Div(
         [
-            dcc.Location(id='url', refresh=True),
-            dcc.Graph(id='pie-chart',),
+            dcc.Location(id="url", refresh=True),
+            dcc.Graph(id="pie-chart"),
         ]
     )
 
@@ -32,28 +27,32 @@ def create_dashboard(server):
 
 
 def init_callbacks(dash_app):
-    @dash_app.callback(
-        Output('pie-chart', 'figure'),
-        [Input('url', 'pathname')]
-    )
+    @dash_app.callback(Output("pie-chart", "figure"), [Input("url", "pathname")])
     def update_pie_chart(pathname):
         # When pathname is like:
         #   "/dashapp/0e8tuDsddlctM6tBDEYPJ2/"
-        # running 
-        #   "/dashapp/0e8tuDsddlctM6tBDEYPJ2/".split("/") 
+        # running
+        #   "/dashapp/0e8tuDsddlctM6tBDEYPJ2/".split("/")
         #   => ['', 'dashapp', '0e8tuDsddlctM6tBDEYPJ2', '']
         # The -2 is two spaces back from the end to get the id
         PATH_SPLIT_INDEX = -2
-        
-        print(f"Pathname:{pathname}")
-        playlist_id = pathname.split('/')[PATH_SPLIT_INDEX]
-        print(f"playlist id:{playlist_id}")    
-        
-        df = playlist_logic.get_tracklist_w_labels(playlist_id)
-        artist_counts = df['artist_name'].value_counts()
 
-                # Create pie chart
-        fig = px.pie(names=artist_counts.index, values=artist_counts.values, title='Artist Counts')
-        fig.update_traces(textposition='inside', textinfo='percent+label', hoverinfo='label+percent')
+        print(f"Pathname:{pathname}")
+        playlist_id = pathname.split("/")[PATH_SPLIT_INDEX]
+        print(f"playlist id:{playlist_id}")
+
+        df = playlist_logic.get_tracklist_w_labels(playlist_id)
+        artist_counts = df["artist_name"].value_counts()
+
+        # Create pie chart
+        fig = px.pie(
+            names=artist_counts.index,
+            values=artist_counts.values,
+            title=CHART_TITLE,
+            template=PLOTLY_THEME,
+        )
+        fig.update_traces(
+            textposition="inside", textinfo="percent+label", hoverinfo="label+percent"
+        )
 
         return fig

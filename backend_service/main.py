2acd45e3894763509f8f5ddb49ba8e19e0fe74a8
Leonardo Franco Antunes <leo.francocrespo@gmail.com>
2024-06-11 21:05:51 +0200

    update

--- backend_service/main.py
+++ backend_service/main.py
@@ -33,4 +33,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    # Note:
+    #   The debug run will trigger here.
+    #   The `fastapi run main.py` defaults to port=8000
+    uvicorn.run(app)
